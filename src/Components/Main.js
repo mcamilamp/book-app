4ea30025affecc2fbf43853968d976e4735a228d
mcamilamp <camimerpa@gmail.com>
2025-03-15 14:39:08 -0500

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,28 @@
+import react from "react";
+const Main = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="row1">
+          <h1>
+            A room without books is like
+            <br /> a body without a soul.
+          </h1>
+        </div>
+
+        <div className="row2">
+          <h2>Find your book</h2>
+          <div className="search">
+            <input type="text" placeholder="Search for books" />
+            <button>
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </div>
+          <img src="./images/bg2.png" alt="img" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
